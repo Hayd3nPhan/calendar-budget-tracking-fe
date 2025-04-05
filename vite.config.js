94f4c10297a58a95fa92dab2d60dc62451efadf9
Max Geek <156335662+Hayd3nPhan@users.noreply.github.com>
2025-04-05 02:33:37 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: './index.html', 
+      input: './index.html'  // updated because index.html is in root
     }
   },
   server: {
-    port: process.env.PORT || 8080, 
+    host: true,                           // required for Render
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true                      // prevent fallback
+  },
+  preview: {
     host: true,
+    port: Number(process.env.PORT) || 4173,
+    strictPort: true
+  }
 })
